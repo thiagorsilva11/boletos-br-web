3f458758b2e2fc7160e392b138d2b7a4fa9aa719
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 12:52:21 -0300

    v0.4.26-cliente-repository

--- app/repositories/cliente_repository.py
+++ app/repositories/cliente_repository.py
@@ -8,8 +8,35 @@ class ClienteRepository(BaseRepository):
 
     @classmethod
     def listar(cls, busca=None):
-
         return super().listar(
             busca,
-            Cliente.representante
+            Cliente.representante,
+        )
+
+    @classmethod
+    def buscar_por_representante(cls, representante):
+        return (
+            cls.model.query
+            .filter(
+                cls.model.representante.ilike(representante)
+            )
+            .first()
+        )
+
+    @classmethod
+    def listar_ativos(cls):
+        return (
+            cls.model.query
+            .filter_by(ativo=True)
+            .order_by(cls.model.representante.asc())
+            .all()
+        )
+
+    @classmethod
+    def listar_inativos(cls):
+        return (
+            cls.model.query
+            .filter_by(ativo=False)
+            .order_by(cls.model.representante.asc())
+            .all()
         )
