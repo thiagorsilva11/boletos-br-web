02bd0366c43d333a262ba91207dfa9b29787dec8
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 12:02:00 -0300

    v0.4.9-model-colaborador

--- app/models/colaborador.py
+++ app/models/colaborador.py
@@ -7,7 +7,8 @@ class Colaborador(BaseModel):
 
     nome = db.Column(
         db.String(150),
-        nullable=False
+        nullable=False,
+        index=True
     )
 
     cpf = db.Column(
@@ -19,5 +20,31 @@ class Colaborador(BaseModel):
         db.String(20)
     )
 
+    ativo = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True,
+        index=True
+    )
+
+    operacoes = db.relationship(
+        "Operacao",
+        backref="colaborador",
+        lazy=True
+    )
+
+    @property
+    def quantidade_operacoes(self):
+        return len(self.operacoes)
+
+    def ativo_status(self):
+        return self.ativo
+
+    def inativo_status(self):
+        return not self.ativo
+
     def __repr__(self):
-        return f"<Colaborador {self.nome}>"
+        return (
+            f"<Colaborador "
+            f"{self.nome}>"
+        )
