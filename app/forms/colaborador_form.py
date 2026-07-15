86f3ec207939cee3942a39bb42802f6e30acdfa4
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 12:55:17 -0300

    v0.4.27-colaborador-form

--- app/forms/colaborador_form.py
+++ app/forms/colaborador_form.py
@@ -1,27 +1,53 @@
 from flask_wtf import FlaskForm
 from wtforms import (
     StringField,
-    SubmitField
+    SubmitField,
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import (
+    DataRequired,
+    Length,
+)
 
 
 class ColaboradorForm(FlaskForm):
 
     nome = StringField(
         "Nome",
-        validators=[DataRequired()]
+        validators=[
+            DataRequired(),
+            Length(max=150),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Nome do colaborador",
+        },
     )
 
     cpf = StringField(
-        "CPF"
+        "CPF",
+        validators=[
+            Length(max=14),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "000.000.000-00",
+        },
     )
 
     telefone = StringField(
-        "Telefone"
+        "Telefone",
+        validators=[
+            Length(max=20),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "(00) 00000-0000",
+        },
     )
 
     submit = SubmitField(
-        "Salvar"
+        "Salvar Colaborador",
+        render_kw={
+            "class": "btn btn-primary",
+        },
     )
-    
