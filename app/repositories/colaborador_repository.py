5f37907c2a2df2986c11f4acc9e2c31a54960341
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 13:08:04 -0300

    v0.4.30-colaborador-repository

--- app/repositories/colaborador_repository.py
+++ app/repositories/colaborador_repository.py
@@ -8,8 +8,35 @@ class ColaboradorRepository(BaseRepository):
 
     @classmethod
     def listar(cls, busca=None):
-
         return super().listar(
             busca,
-            Colaborador.nome
+            Colaborador.nome,
+        )
+
+    @classmethod
+    def buscar_por_nome(cls, nome):
+        return (
+            cls.model.query
+            .filter(
+                cls.model.nome.ilike(nome)
+            )
+            .first()
+        )
+
+    @classmethod
+    def listar_ativos(cls):
+        return (
+            cls.model.query
+            .filter_by(ativo=True)
+            .order_by(cls.model.nome.asc())
+            .all()
+        )
+
+    @classmethod
+    def listar_inativos(cls):
+        return (
+            cls.model.query
+            .filter_by(ativo=False)
+            .order_by(cls.model.nome.asc())
+            .all()
         )
