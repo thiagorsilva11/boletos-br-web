f0312389ea08235aea8cd99991123dee43df32e8
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 12:44:24 -0300

    v0.4.22-fabrica-repository

--- app/repositories/fabrica_repository.py
+++ app/repositories/fabrica_repository.py
@@ -8,8 +8,44 @@ class FabricaRepository(BaseRepository):
 
     @classmethod
     def listar(cls, busca=None):
-
         return super().listar(
             busca,
             Fabrica.nome
         )
+
+    @classmethod
+    def buscar_por_nome(cls, nome):
+        return (
+            cls.model.query
+            .filter(
+                cls.model.nome.ilike(nome)
+            )
+            .first()
+        )
+
+    @classmethod
+    def listar_ativas(cls):
+        return (
+            cls.model.query
+            .filter_by(ativa=True)
+            .order_by(cls.model.nome.asc())
+            .all()
+        )
+
+    @classmethod
+    def listar_inativas(cls):
+        return (
+            cls.model.query
+            .filter_by(ativa=False)
+            .order_by(cls.model.nome.asc())
+            .all()
+        )
+
+    @classmethod
+    def obter_percentual_comissao(cls, fabrica_id):
+        fabrica = cls.buscar_por_id(fabrica_id)
+
+        if fabrica is None:
+            return 0
+
+        return fabrica.percentual_comissao
