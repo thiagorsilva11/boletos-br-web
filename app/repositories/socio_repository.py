45d001290a8204e57f1c2c44e1ed4c2df705745a
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 13:19:01 -0300

    v0.4.33-socio-repository

--- app/repositories/socio_repository.py
+++ app/repositories/socio_repository.py
@@ -8,9 +8,43 @@ class SocioRepository(BaseRepository):
 
     @classmethod
     def listar(cls, busca=None):
-
         return super().listar(
             busca,
-            Socio.nome
+            Socio.nome,
         )
-        
+
+    @classmethod
+    def buscar_por_nome(cls, nome):
+        return (
+            cls.model.query
+            .filter(
+                cls.model.nome.ilike(nome)
+            )
+            .first()
+        )
+
+    @classmethod
+    def listar_ativos(cls):
+        return (
+            cls.model.query
+            .filter_by(ativo=True)
+            .order_by(cls.model.nome.asc())
+            .all()
+        )
+
+    @classmethod
+    def listar_inativos(cls):
+        return (
+            cls.model.query
+            .filter_by(ativo=False)
+            .order_by(cls.model.nome.asc())
+            .all()
+        )
+
+    @classmethod
+    def listar_por_saldo(cls):
+        return (
+            cls.model.query
+            .order_by(cls.model.saldo.desc())
+            .all()
+        )
