d9282ec53474629cd03f1e43b6a2c877956f4670
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 11:05:45 -0300

    v0.4.4-conta-receber-repository

--- app/repositories/conta_receber_repository.py
+++ app/repositories/conta_receber_repository.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from app.models.conta_receber import ContaReceber
 from app.repositories.base_repository import BaseRepository
 
@@ -7,11 +9,19 @@ class ContaReceberRepository(BaseRepository):
     model = ContaReceber
 
     @classmethod
+    def buscar_por_boleto(cls, boleto_id):
+        return (
+            cls.model.query
+            .filter_by(boleto_id=boleto_id)
+            .first()
+        )
+
+    @classmethod
     def listar_em_aberto(cls):
         return (
             cls.model.query
             .filter_by(status=0)
-            .order_by(cls.model.data_prevista)
+            .order_by(cls.model.data_prevista.asc())
             .all()
         )
 
@@ -26,20 +36,28 @@ class ContaReceberRepository(BaseRepository):
 
     @classmethod
     def listar_vencidas(cls):
-        from datetime import date
-
         return (
             cls.model.query
             .filter(
                 cls.model.status == 0,
                 cls.model.data_prevista < date.today()
             )
-            .order_by(cls.model.data_prevista)
+            .order_by(cls.model.data_prevista.asc())
             .all()
         )
 
     @classmethod
-    def listar_por_boleto(cls, boleto_id):
-        return cls.model.query.filter_by(
-            boleto_id=boleto_id
-        ).first()
+    def listar_por_periodo(
+        cls,
+        data_inicial,
+        data_final
+    ):
+        return (
+            cls.model.query
+            .filter(
+                cls.model.data_prevista >= data_inicial,
+                cls.model.data_prevista <= data_final
+            )
+            .order_by(cls.model.data_prevista.asc())
+            .all()
+        )
