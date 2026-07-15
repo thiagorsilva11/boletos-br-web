3bbc0e96b12a134ae529d1b2c21328665b948db9
thiagorsilva11 <thiagorsilva1@hotmail.com>
2026-07-15 12:46:30 -0300

    v0.4.23-cliente-form

--- app/forms/cliente_form.py
+++ app/forms/cliente_form.py
@@ -2,37 +2,89 @@ from flask_wtf import FlaskForm
 from wtforms import (
     StringField,
     TextAreaField,
-    SubmitField
+    SubmitField,
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import (
+    DataRequired,
+    Length,
+)
 
 
 class ClienteForm(FlaskForm):
 
     representante = StringField(
         "Representante",
-        validators=[DataRequired()]
+        validators=[
+            DataRequired(),
+            Length(max=150),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Nome do representante",
+        },
     )
 
     responsavel = StringField(
         "Responsável",
-        validators=[DataRequired()]
+        validators=[
+            DataRequired(),
+            Length(max=150),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Nome do responsável",
+        },
     )
 
     cpf = StringField(
-        "CPF"
+        "CPF",
+        validators=[
+            Length(max=14),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "000.000.000-00",
+        },
     )
 
     telefone = StringField(
-        "Telefone"
+        "Telefone",
+        validators=[
+            Length(max=20),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "(00) 00000-0000",
+        },
     )
 
     endereco = StringField(
-        "Endereço"
+        "Endereço",
+        validators=[
+            Length(max=250),
+        ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Endereço (opcional)",
+        },
     )
 
     observacoes = TextAreaField(
-        "Observações"
+        "Observações",
+        validators=[
+            Length(max=500),
+        ],
+        render_kw={
+            "class": "form-control",
+            "rows": 3,
+            "maxlength": 500,
+            "placeholder": "Observações (opcional)",
+        },
     )
 
-    submit = SubmitField("Salvar")
+    submit = SubmitField(
+        "Salvar Representante",
+        render_kw={
+            "class": "btn btn-primary",
+        },
+    )
